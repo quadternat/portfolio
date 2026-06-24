7762b12982cd354f10a918cff70e8824f5a26039
quadternat <quadternat@gmail.com>
2026-06-24 16:57:24 +0100

    Update CalcV2.py

--- calculator/CalcV2.py
+++ calculator/CalcV2.py
@@ -16,18 +16,20 @@ listOfViableOperations = {
 
 listOfViableSpecialOperations = {
     "ls": "lists all functions which are usable",
-    "help": "provides description of all operations and which Operation it is (sp or reg)"
+    "help": "provides description of all operations and which Operation it is (sp or reg)",
+    "exit": "exits the program"
 }
+
 while running:
     if menu == 0:
         print('''welcome to calculator Version 2 - if you need help just use "help" ''')
         menu += 1
         
     inputEQ = input("> ")
-    
-    if inputEQ not in listOfViableOperations and inputEQ not in listOfViableSpecialOperations:
-        print("invalid input")
 
+    if inputEQ == "exit":
+        running = False
+    
     if inputEQ == "ls":
         print("math")
         for i, op in enumerate(listOfViableOperations, 1):
@@ -37,4 +39,14 @@ while running:
             print(f"{i}. {op}")
 
     if inputEQ == "help":
-        print("help will show")
+        print("help will show every operation")
+        for operation in listOfViableOperations:
+            print(operation, listOfViableOperations[operation])
+        for operation in listOfViableSpecialOperations:
+            print(operation, listOfViableSpecialOperations[operation])
+
+    if len(inputEQ) == 2 and inputEQ[0] == "help":
+        print("help func used")
+
+    if inputEQ not in listOfViableOperations and inputEQ not in listOfViableSpecialOperations:
+        print("invalid input")
